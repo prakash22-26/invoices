8939bd341e1b36ce2e64408b99783d70299ab629
prakash22-26 <prakashkumarshah156@gmail.com>
2025-04-28 14:05:13 +0530

    Update Register.js

--- invoice-system-frontends/src/pages/Register.js
+++ invoice-system-frontends/src/pages/Register.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 import "./Register.css"; // 👈 Import the external CSS
